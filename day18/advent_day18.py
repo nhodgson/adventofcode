9afc85bd9631cf2b4fccdfa2b3da864cf34bf20e
Neil Hodgson <neil.n.hodgson@maersk.com>
2020-12-22 23:42:23 +0100

    solution for day18 part 2

--- day18/advent_day18.py
+++ day18/advent_day18.py
@@ -1,5 +1,11 @@
 import operator as op
 
+def prod(vals):
+    ans = 1
+    for v in vals:
+        ans *= v
+    return ans
+
 def get_tests(fname='advent_day18_test.txt'):
     with open(fname) as fid:
         out = []
@@ -38,24 +44,42 @@ def parse(s):
     for i in range(0,len(s)-2,2):
         b = int(s[i+2])
         val = OPS_MAP[s[i+1]](val,b)
-
     return val
+
+def parse2(s):
+    s = s.split('*')
+    vals = [eval(x) for x in s]
+    return prod(vals)
 
 def evl(s):
     while find_parens(s) != []:
         p = find_parens(s)[0]
         i, j = [int(x) for x in p]
-        print(i,j,s[i+1:j])
         ans = parse(s[i+1:j])
         s = s.replace(s[i:j+1], str(ans))
     return parse(s)
 
-def run_tests():
-    tests = get_tests()
+def evl2(s):
+    while find_parens(s) != []:
+        p = find_parens(s)[0]
+        i, j = [int(x) for x in p]
+        ans = parse2(s[i+1:j])
+        s = s.replace(s[i:j+1], str(ans))
+    return parse2(s)
+
+def run_tests(fname):
+    tests = get_tests(fname)
     for test in tests:
         ans = test[1]
         s = test[0]
         assert evl(s) == ans
+
+def run_tests2(fname):
+    tests = get_tests(fname)
+    for test in tests:
+        ans = test[1]
+        s = test[0]
+        assert evl2(s) == ans
 
 def part1():
     lines = get_input()
@@ -64,6 +88,18 @@ def part1():
         ans.append(evl(line))
     return sum(ans)
 
+def part2():
+    lines = get_input()
+    ans = []
+    for line in lines:
+        ans.append(evl2(line))
+    return sum(ans)
+
+run_tests('advent_day18_test.txt')
+run_tests2('advent_day18_test2.txt')
 
 ans = part1()
 print(f'The answer to part 1 is: {ans}')
+
+ans = part2()
+print(f'The answer to part 2 is: {ans}')
